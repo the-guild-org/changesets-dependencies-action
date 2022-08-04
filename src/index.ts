e4976f8159ba28166c15d9c81c8d64122bb03c03
Dotan Simha <dotansimha@gmail.com>
2022-08-04 11:52:20 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 import { setupGitCredentials, setupGitUser } from "./utils";
+import fetch from "node-fetch";
+
+async function fetchFile(
+  pat: string,
+  file: {
+    path: string;
+    owner: string;
+    repo: string;
+    ref: string;
+  }
+) {
+  return await fetch(
+    `https://raw.githubusercontent.com/${file.owner}/${file.repo}/${file.ref}/${file.path}`,
+    {
+      headers: {
+        Authorization: `Token ${pat}`,
+      },
+    }
+  ).catch((err) => {
+    console.error(err);
+
+    throw err;
+  });
+}
+
+async function fetchJsonFile(
+  pat: string,
+  file: {
+    path: string;
+    owner: string;
+    repo: string;
+    ref: string;
+  }
+) {
+  return await fetchFile(pat, file)
+    .then((x) => x.json())
+    .catch(() => null);
+}
 
 (async () => {
   let githubToken = process.env.GITHUB_TOKEN;
@@ -9,10 +48,73 @@ import { setupGitCredentials, setupGitUser } from "./utils";
     return;
   }
 
+  const octokit = github.getOctokit(githubToken);
+
   console.log("setting GitHub User");
   await setupGitUser();
   console.log("setting GitHub credentials");
   await setupGitCredentials(githubToken);
+
+  const issueContext = github.context.issue;
+
+  if (!issueContext?.number) {
+    console.debug(github.context);
+    core.warning(`Failed to locate a PR associated with the Action context:`);
+    core.setFailed(`Failed to locate a PR associated with the Action context`);
+
+    return;
+  }
+
+  const { data: changes } = await octokit.rest.git.getTree({
+    ...github.context.repo,
+    recursive: "1",
+    tree_sha: github.context.ref,
+  });
+
+  const filesToScan = changes.tree
+    .map((item) =>
+      item.path && item.path.endsWith("/package.json") ? item.path : null
+    )
+    .filter(Boolean);
+
+  console.debug(
+    `Found total of ${filesToScan.length} changed package.json files to check:`,
+    filesToScan.join(", ")
+  );
+
+  if (filesToScan.length) {
+    const filesContent = await Promise.all(
+      filesToScan.map(async (filePath) => {
+        try {
+          const newPackageFile = await fetchJsonFile(githubToken!, {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            path: filePath!,
+            ref: github.context.ref,
+          });
+
+          return {
+            filePath,
+            newPackageFile,
+          };
+          // const oldPackageFile = await fetchJsonFile(githubToken!, {
+          //   owner: github.context.repo.owner,
+          //   repo: github.context.repo.repo,
+          //   path: filePath!,
+          //   ref: github.context.payload.pull_request,
+          // });
+        } catch (e) {
+          console.warn(`Failed to fetch package.json file: ${filePath}`, e);
+
+          return null;
+        }
+      })
+    );
+
+    console.log(filesContent);
+  } else {
+    core.info(`Failed to locate any package.json files to scan in the PR`);
+  }
 })().catch((err) => {
   console.error(err);
   core.setFailed(err.message);
